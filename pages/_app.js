2b2a19d88ca472aa87118b9bb76cec7c9b232f62
Steve Booth <scriptmcuk@gmail.com>
2021-01-26 08:13:21 +0000

    added margin bottom to header and italic 400 font

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '@fontsource/roboto';
 import '@fontsource/roboto/900.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
 import '@fontsource/roboto/100-italic.css';
 
 function MyApp({ Component, pageProps }) {
