8f146452920a1e209eb7a0a8f5f0383c0b1e328e
Steve Booth <scriptmcuk@gmail.com>
2021-02-01 09:04:56 +0000

    Added mobile nav component

--- components/Layout.js
+++ components/Layout.js
@@ -11,13 +11,17 @@ function Layout({ children, pageTitle }) {
         <title>{pageTitle}</title>
       </Head>
       <div className='flex flex-col min-h-screen bg-gray-900 text-gray-400'>
-        <header className='w-full h-20 mt-8  flex  justify-center items-end bg-gray-900'>
-          <div className='group w-11/12 md:w-full max-w-2xl flex flex-row justify-between items-stretch'>
+        <header className='w-full h-20 mt-8  flex  justify-center items-end bg-gray-900 '>
+          <nav className='group w-11/12 md:w-full max-w-2xl flex flex-row justify-between items-stretch'>
             <Link href='/'>
-              <div className='text-5xl text-primary cursor-pointer'>SB</div>
+              <div className='text-5xl z-50 text-primary cursor-pointer'>
+                SB
+              </div>
             </Link>
-            <AppNav />
-          </div>
+            <HamburgerBtn />
+            {/* <AppNav /> */}
+            <MobileNav />
+          </nav>
         </header>
         <main className='w-11/12 md:w-full max-w-2xl mx-auto my-8 flex-grow bg-gray-900 font-sans'>
           {children}
@@ -34,22 +38,72 @@ function Layout({ children, pageTitle }) {
   );
 }
 
+function MobileNav() {
+  return (
+    <div className='fixed top-0 left-0 bg-gray-900 w-full h-screen'>
+      <ul className=' h-full flex flex-col justify-center items-center text-cream font-sans'>
+        <li className='my-8 mx-8'>
+          <ActiveLink href='/work' activeClassName='nav-active'>
+            <a className='mob-nav-item'>work</a>
+          </ActiveLink>
+        </li>
+
+        <li className='my-8 mx-8'>
+          <ActiveLink href='/' activeClassName='nav-active'>
+            <a className='mob-nav-item'>words</a>
+          </ActiveLink>
+        </li>
+
+        <li className='my-8 mx-8'>
+          <ActiveLink href='/bio' activeClassName='nav-active'>
+            <a className='mob-nav-item'>bio</a>
+          </ActiveLink>
+        </li>
+
+        <li className='my-8 mx-8'>
+          <ActiveLink href='/about' activeClassName='nav-active'>
+            <a className='mob-nav-item'>about</a>
+          </ActiveLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
 function AppNav() {
   return (
-    <nav className=' text-cream font-sans flex items-end '>
-      <ActiveLink href='/work' activeClassName='nav-active'>
-        <a className='nav-item'>work</a>
-      </ActiveLink>
-      <ActiveLink href='/' activeClassName='nav-active'>
-        <a className='nav-item'>words</a>
-      </ActiveLink>
-      <ActiveLink href='/bio' activeClassName='nav-active'>
-        <a className='nav-item'>bio</a>
-      </ActiveLink>
-      <ActiveLink href='/about' activeClassName='nav-active'>
-        <a className='nav-item'>about</a>
-      </ActiveLink>
-    </nav>
+    <ul className=' md:flex md:flex-row text-cream font-sans md:items-end'>
+      <li>
+        <ActiveLink href='/work' activeClassName='nav-active'>
+          <a className='nav-item'>work</a>
+        </ActiveLink>
+      </li>
+      <li>
+        <ActiveLink href='/' activeClassName='nav-active'>
+          <a className='nav-item'>words</a>
+        </ActiveLink>
+      </li>
+      <li>
+        <ActiveLink href='/bio' activeClassName='nav-active'>
+          <a className='nav-item'>bio</a>
+        </ActiveLink>
+      </li>
+      <li>
+        <ActiveLink href='/about' activeClassName='nav-active'>
+          <a className='nav-item'>about</a>
+        </ActiveLink>
+      </li>
+    </ul>
+  );
+}
+
+function HamburgerBtn({ isHidden, isOpen }) {
+  return (
+    <div className='w-10 z-50 flex flex-col justify-around'>
+      <div className='h-1.5 w-full rounded bg-cream transition-transform duration-200 transform rotate-45 scale-x-110 origin-left'></div>
+      <div className='h-1.5 w-full rounded bg-cream transition  duration-200 opacity-0'></div>
+      <div className='h-1.5 w-full rounded bg-cream transition-transform duration-200 transform -rotate-45 scale-x-110 origin-left'></div>
+    </div>
   );
 }
 
