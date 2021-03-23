dbac16614a837c04e82a666d1bca25c7506c17b7
Stephen Booth <stephenboothdev@gmail.com>
2021-03-23 09:28:43 +0000

    Added social links to footer

--- components/SocialLink.js
+++ components/SocialLink.js
@@ -4,14 +4,14 @@ const SocialLink = ({ Icon, text, href }) => {
   return (
     <button className="bg-pink hover:bg-primary text-gray-900 py-2 pl-4 pr-4 rounded-sm mx-2">
       <Icon color="rgb(17,24,39)" />
-      {text ?? (
+      {text ? (
         <>
           <span className="opacity-0">..</span>
           {text}
         </>
+      ) : (
+        ""
       )}
-
-      {text}
     </button>
   );
 };
